5691f0b6612087f0c0659010122c0585857120c3
Daria Maracheva <dmaracheva@gmail.com>
2017-12-28 22:31:53 +0300

    with №2

--- file.py
+++ file.py
@@ -30,29 +30,73 @@ def cook_book_collector():
     return cook_book
 cook_book_collector()
 
-#
-# def get_shop_list_by_dishes(dishes, person_count):
-#     shop_list = {}
-#     for dish in dishes:
-#         for ingredient in cook_book[dish]:
-#             new_shop_list_item = dict(ingredient)
-#             new_shop_list_item['quantity'] *= person_count
-#             if new_shop_list_item['ingredient_name'] not in shop_list:
-#                 shop_list[new_shop_list_item['ingredient_name']] = new_shop_list_item
-#             else:
-#                 shop_list[new_shop_list_item['ingredient_name']]['quantity'] += new_shop_list_item['quantity']
-#     return shop_list
-#
-# def print_shop_list(shop_list):
-#     for shop_list_item in shop_list.values():
-#         print('{} {} {}'.format(shop_list_item['ingredient_name'], shop_list_item['quantity'], shop_list_item['measure']))
-#
-# def create_shop_list():
-#     # person_count = int(input('Введите количество человек: '))
-#     person_count = 3
-#     # dishes = input('Введите блюда в расчете на одного человека (через запятую): ').lower().split(', ')
-#     dishes = 'стейк, салат'.lower().split(', ')
-#     shop_list = get_shop_list_by_dishes(dishes, person_count)
-#     print_shop_list(shop_list)
-#
-# create_shop_list()
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        for ingredient in cook_book[dish]:
+            new_shop_list_item = dict(ingredient)
+            new_shop_list_item['quantity'] *= person_count
+            if new_shop_list_item['ingredient_name'] not in shop_list:
+                shop_list[new_shop_list_item['ingredient_name']] = new_shop_list_item
+            else:
+                shop_list[new_shop_list_item['ingredient_name']]['quantity'] += new_shop_list_item['quantity']
+    return shop_list
+
+def print_shop_list(shop_list):
+    for shop_list_item in shop_list.values():
+        print('{} {} {}'.format(shop_list_item['ingredient_name'], shop_list_item['quantity'], shop_list_item['measure']))
+
+def create_shop_list():
+    # person_count = int(input('Введите количество человек: '))
+    person_count = 3
+    # dishes = input('Введите блюда в расчете на одного человека (через запятую): ').lower().split(', ')
+    dishes = 'стейк, салат'.lower().split(', ')
+    shop_list = get_shop_list_by_dishes(dishes, person_count)
+    print_shop_list(shop_list)
+
+create_shop_list()
+
+# Задача №2
+# Напишите, для чего используются типы данных: json, xml, yaml.
+# JSON (англ. JavaScript Object Notation) — текстовый формат обмена данными, основанный на JavaScript и обычно используемый именно с этим языком.
+# Формат считается языконезависимым и может использоваться практически с любым языком программирования.
+#  var earth =
+# {
+#  "planet" :
+#  {
+#   "name" : "earth",
+#   "type" : "small",
+#   "info":
+#   [
+#    "Earth is a small planet, third from the sun",
+#    "Surface coverage of water is roughly two-thirds",
+#    "Exhibits a remarkable diversity of climates and landscapes"
+#   ]
+#  }
+#  };
+
+# xml
+# XML – это eXtensible Markup Language, что в переводе значит «расширенный язык разметки».
+# Фактически, это способ записи данных в структурированном виде, который будет читаем для пользователя,
+# но при этом удобен для обработки программному обеспечению. XML документ содержит один или более элементов,
+#  разделённых открывающими и закрывающими тегами:
+
+# <str>Hello!</str>
+
+# YAML — легкочитаемый формат сериализации данных, концептуально близкий к языкам разметки,
+# но ориентированный на удобство ввода-вывода типичных структур данных многих языков программирования.
+
+# ---
+#  -
+#     - PRIVMSG
+#     - newUri
+#     - '^http://.*'
+#  -
+#     - PRIVMSG
+#     - deleteUri
+#     - ^delete.*
+#  -
+#     - PRIVMSG
+#     - randomUri
+#     - ^random.*
